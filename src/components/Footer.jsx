b0cc4d4a51fc84324ee6130cd27d2cf5609d8722
abi26anamo <abianamo282@gmail.com>
2023-06-04 15:31:12 +0300

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = ()=>{
-     return <div className="bg-gray-800 text-sm p-4 text-center text-white ">
-          Copyright © 2023 Abinet Anamo. All Rights Reserved.
-     </div>;
-};
-
-export default Footer;
